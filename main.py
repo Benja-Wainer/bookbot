4664d48450b72a9694c5fa8723d2bc7d07443c6a
Benjamin Wainer <123282375+Benja-Wainer@users.noreply.github.com>
2025-01-13 13:24:31 +0900

    Adds function that returns the book's word count.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
     book_file = "books/frankenstein.txt"
     text = get_text(book_file)
-    print(text)
+    word_count = count_words(text)
+    # print(text)
+    print(f"The books has {word_count} words.")
 
 def get_text(book_file):
     with open(book_file) as f:
         text = f.read()
         return text
+
+def count_words(text):
+    splitted_text = text.split()
+    return len(splitted_text)
 
 main()
